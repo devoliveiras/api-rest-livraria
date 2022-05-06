346bc5e1b0d74bdd1900d05902bcceb27cad2d36
devoliveiras <guilhermeao@outlook.com>
2022-05-06 16:07:12 -0300

    Adicionado novo Schema para autores e consulta personalizada pela requisição

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js"
 import livros from "./models/Livro.js"
+import autoress from "./models/Autor.js"
 import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, 'Erro ao conectar com a base de dados'))
